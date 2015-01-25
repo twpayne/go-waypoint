d6ee65ddb3fc613b68b36d1b33fb83018579dba2
Tom Payne <twpayne@gmail.com>
2015-01-25 19:47:34 +0100

    Add dmsh.DMH

--- internal/dmsh/dmsh.go
+++ internal/dmsh/dmsh.go
@@ -8,6 +8,18 @@ func D(d, m, s float64) float64 {
 	return d + m/60 + s/3600
 }
 
+func DMH(x float64, hs string) (d int, m float64, h uint8) {
+	if x < 0 {
+		h = hs[1]
+		x = -x
+	} else {
+		h = hs[0]
+	}
+	d = int(x)
+	m = math.Mod(60*x, 60)
+	return
+}
+
 func DMSH(x float64, hs string) (d, m int, s float64, h uint8) {
 	if x < 0 {
 		h = hs[1]
