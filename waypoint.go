5e4227e4e9720b67037ae3473f3eec58d8e5415a
Tom Payne <twpayne@gmail.com>
2015-03-01 19:58:47 +0100

    Factor out GeoJSON

--- waypoint.go
+++ waypoint.go
@@ -1,7 +1,6 @@
 package waypoint
 
 import (
-	"encoding/json"
 	"fmt"
 	"image/color"
 )
@@ -61,36 +60,3 @@ func Equal(t1, t2 *T) bool {
 }
 
 type Collection []*T
-
-func (w *T) MarshalJSON() ([]byte, error) {
-	o := map[string]interface{}{
-		"id": w.Id,
-		"geometry": map[string]interface{}{
-			"type":        "Point",
-			"coordinates": []float64{w.Latitude, w.Longitude, w.Altitude},
-		},
-	}
-	properties := make(map[string]interface{})
-	if w.Color != nil {
-		r, g, b, _ := w.Color.RGBA()
-		properties["color"] = fmt.Sprintf("#%02x%02x%02x", r/0x101, g/0x101, b/0x101)
-	}
-	if w.Description != "" {
-		properties["description"] = w.Description
-	}
-	if w.Radius > 0 {
-		properties["radius"] = w.Radius
-	}
-	if len(properties) > 0 {
-		o["properties"] = properties
-	}
-	return json.Marshal(o)
-}
-
-func (wc Collection) MarshalJSON() ([]byte, error) {
-	o := map[string]interface{}{
-		"type":     "FeatureCollection",
-		"features": []*T(wc),
-	}
-	return json.Marshal(o)
-}
